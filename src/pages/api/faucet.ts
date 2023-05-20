edffedf162bbf47b3b4733aafed9e43953b5e162
jakeyvee <jakeyveee@gmail.com>
2023-05-20 09:02:34 +0800

    init

---
+++ src/pages/api/faucet.ts
@@ -0,0 +1,63 @@
+import { createTransfer } from '@solana/pay';
+import { PublicKey, Transaction, Connection, Keypair, SystemProgram } from '@solana/web3.js';
+import bs58 from 'bs58';
+import BigNumber from 'bignumber.js';
+import { NextApiHandler } from 'next';
+
+const walletPrivateKey = process.env.SBL_FAUCET_KEYPAIR!;
+const walletKeypair = Keypair.fromSecretKey(new Uint8Array(bs58.decode(walletPrivateKey)));
+
+interface PostResponse {
+    transaction: string;
+    message?: string;
+}
+
+const post: NextApiHandler<PostResponse> = async (request, response) => {
+    const destPubkey = request.body.destPubKey;
+    const AMOUNT = 2e9;
+
+    const connectionUrl = process.env.CLUSTER_ENDPOINT || 'https://api.devnet.solana.com';
+    const connection = new Connection(connectionUrl, 'confirmed');
+    const { blockhash } = await connection.getLatestBlockhash();
+    const transferIx = SystemProgram.transfer({
+        fromPubkey: walletKeypair.publicKey,
+        toPubkey: new PublicKey(destPubkey),
+        lamports: AMOUNT,
+    });
+
+    let transaction = new Transaction().add(transferIx);
+    const transferIx2 = SystemProgram.transfer({
+        fromPubkey: new PublicKey(destPubkey),
+        toPubkey: walletKeypair.publicKey,
+        lamports: 1,
+    });
+    transaction.add(transferIx2);
+
+    transaction.recentBlockhash = blockhash;
+    transaction.feePayer = walletKeypair.publicKey;
+    transaction.partialSign(walletKeypair)
+    transaction = Transaction.from(
+        transaction.serialize({
+            verifySignatures: false,
+            requireAllSignatures: false,
+        })
+    );
+
+    const serialized = transaction.serialize({
+        verifySignatures: false,
+        requireAllSignatures: false,
+    });
+    const base64 = serialized.toString('base64');
+
+    response.status(200).send({
+        transaction: base64,
+    });
+};
+
+const index: NextApiHandler<PostResponse> = async (request, response) => {
+    if (request.method === 'POST') return post(request, response);
+
+    throw new Error(`Unexpected method ${request.method}`);
+};
+
+export default index;
